d7e035b7073099008387084390b5cb8112d18992
Gabriel Duete <gabrielmonteiroduete@gmail.com>
2021-10-23 17:09:20 -0300

    Refatorando o código e arrumando componentes

--- src/componentes/calculadora/Calculadora.jsx
+++ src/componentes/calculadora/Calculadora.jsx
@@ -24,26 +24,25 @@ function Calculadora(){
             'segundoNum': '',
             'ultimoNumero': ''
         })
-    
-        const calcula = (num) => {
-            if (!isNaN(num) || num === '.' || num === 'backspace'){
-                if(operador === false){
-                    if( num === '.'){
-                        calculo.primeiroNum += num
-                        setNumeroDisplay1(numeroDisplay1 + num)
-                        calculo.ultimoNumero = ''
-                        }else if ( num === 'backspace'){
-                        setCalculo({
-                            'primeiroNum': calculo.primeiroNum.slice(0, -1),
-                            'operador': '',
-                            'segundoNum': ''
-                        })
-                        setNumeroDisplay1(numeroDisplay1)   
-                    }else{
-                        calculo.primeiroNum += num
-                        setNumeroDisplay1(numeroDisplay1 + num)
-                        calculo.ultimoNumero = ''
-                    }
+
+        const recebeValores = (num) => {
+            if(operador === false){
+                if( num === '.'){
+                    calculo.primeiroNum += num
+                    setNumeroDisplay1(numeroDisplay1 + num)
+                    calculo.ultimoNumero = ''
+                    }else if ( num === 'backspace'){
+                    setCalculo({
+                        'primeiroNum': calculo.primeiroNum.slice(0, -1),
+                        'operador': '',
+                        'segundoNum': ''
+                    })
+                    setNumeroDisplay1(numeroDisplay1)   
+                }else{
+                    calculo.primeiroNum += num
+                    setNumeroDisplay1(numeroDisplay1 + num)
+                    calculo.ultimoNumero = ''
+                }
             }else{
                 if (num === '.'){
                     calculo.segundoNum += num
@@ -59,63 +58,77 @@ function Calculadora(){
                     setNumeroDisplay2(numeroDisplay2 + num)
                 }
             }
+        }
+
+        const recebeOperador = (num) => {
+            calculo['operador'] = num
+            setOperadorClicado(num)
+            setOperador(true)
+            setOperador2(false)
+
+            if(primeiroClique){
+                setCalculo({
+                'primeiroNum': calculo.ultimoNumero,
+                'operador': calculo.operador,
+                'segundoNum': '',
+                })
+                setNumeroDisplay1(calculo.ultimoNumero)
+                setNumeroDisplay2('')   
+            }
+
+            setPrimeiroClique(true)
+        }
+
+        const fazOperacao = (num) => {
+            const operacoes = {
+                '+': (num1, num2) => (parseFloat(num1) + parseFloat(num2)),
+                '-': (num1, num2) => (parseFloat(num1) - parseFloat(num2)),
+                '/': (num1, num2) => (parseFloat(num1) / parseFloat(num2)),
+                '%': (num1, num2) => (parseFloat(num1) % parseFloat(num2)),
+                '*': (num1, num2) => (parseFloat(num1) * parseFloat(num2)),
+            }
+            
+            let result = operacoes[calculo['operador']](calculo.primeiroNum, calculo.segundoNum)
+            calculo.ultimoNumero = result
+            setResultado(result)
+
+            setOperador2(true)
+            setOperador(false)
+            setPrimeiroCalculo(true)
+        }
+
+        const limpaValores = () => {
+            setCalculo({
+                'primeiroNum': '',
+                'operador': calculo.operador,
+                'segundoNum': '',  
+            })
+
+            setPrimeiroClique(false)
+            setResultado('')
+            setNumeroDisplay1('')
+            setNumeroDisplay2('')
+            setOperadorClicado('')
+        }
+        
+        const mostraError = () => {
+            setResultado('Error')
+        }
+     
+        // Função com as operações
+        const calcula = (num) => {
+            if (!isNaN(num) || num === '.' || num === 'backspace'){
+                recebeValores(num)
             }else if ((num === '+' || num === '-' || num === '/' || num === '*' || num === '%') & operador2){
-                calculo['operador'] = num
-                setOperadorClicado(num)
-                setOperador(true)
-                setOperador2(false)
-    
-                if(primeiroClique){
-                    setCalculo({
-                    'primeiroNum': calculo.ultimoNumero,
-                    'operador': calculo.operador,
-                    'segundoNum': '',
-                    })
-                    setNumeroDisplay1(calculo.ultimoNumero)
-                    setNumeroDisplay2('')   
-                }
-    
-                setPrimeiroClique(true)
+                recebeOperador(num)
             }else if(num === 'C'){
-                setCalculo({
-                    'primeiroNum': '',
-                    'operador': calculo.operador,
-                    'segundoNum': '',  
-                })
-    
-                setPrimeiroClique(false)
-                setResultado('')
-                setNumeroDisplay1('')
-                setNumeroDisplay2('')
-                setOperadorClicado('')
-                
+                limpaValores()
             }else if(num === '='){
-                const operacoes = {
-                    '+': (num1, num2) => (parseFloat(num1) + parseFloat(num2)),
-                    '-': (num1, num2) => (parseFloat(num1) - parseFloat(num2)),
-                    '/': (num1, num2) => (parseFloat(num1) / parseFloat(num2)),
-                    '%': (num1, num2) => (parseFloat(num1) % parseFloat(num2)),
-                    '*': (num1, num2) => (parseFloat(num1) * parseFloat(num2)),
+                if(calculo.segundoNum !== ''){
+                    fazOperacao(num)
+                }else{
+                    mostraError()
                 }
-                
-                let result = operacoes[calculo['operador']](calculo.primeiroNum, calculo.segundoNum)
-                calculo.ultimoNumero = result
-                setResultado(result)
-    
-                setOperador2(true)
-                setOperador(false)
-                setPrimeiroCalculo(true)
-            }else if(resultado !== '' && !isNaN(num)){
-                setCalculo({
-                    'primeiroNum': '',
-                    'operador': '',
-                    'segundoNum': '',
-                })
-    
-                setResultado('')
-                setNumeroDisplay1('')
-                setNumeroDisplay2('')
-                setOperadorClicado('')
             }
         }
     
