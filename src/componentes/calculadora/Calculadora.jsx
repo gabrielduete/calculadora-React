15b4ca7f03f38139f4325ab457c9479c0292c979
Gabriel Duete <gabrielmonteiroduete@gmail.com>
2021-10-23 17:48:49 -0300

    Refatorando o código da calculadora.jsx

--- src/componentes/calculadora/Calculadora.jsx
+++ src/componentes/calculadora/Calculadora.jsx
@@ -17,7 +17,8 @@ function Calculadora(){
         const [primeiroClique, setPrimeiroClique] = useState(false)
         const [primeiroCalculo, setPrimeiroCalculo] = useState(false)
     
-        // State com o calculo
+        // State que recebe o Primeiro valor, o operador, o segundo valor e faz a operação
+        // 'ultimoNumero' é o resultado da ultima operação
         const [calculo, setCalculo] = useState({
             'primeiroNum': '',
             'operador': '',
@@ -25,47 +26,61 @@ function Calculadora(){
             'ultimoNumero': ''
         })
 
+        // Função para receber os valores pra exibir no Display e guardar no state Calculo
         const recebeValores = (num) => {
             if(operador === false){
-                if( num === '.'){
+                // Esse IF vai verificar se ja tiver um resultado na tela e clicar em um número, ele vai limpar e colocar o numero digitado
+                if(primeiroCalculo){
+                    limpaValores(num, true)
+                    setPrimeiroCalculo(false)
+                }if( num === '.'){
+                    // Adicionando ponto para valor Float
                     calculo.primeiroNum += num
                     setNumeroDisplay1(numeroDisplay1 + num)
                     calculo.ultimoNumero = ''
-                    }else if ( num === 'backspace'){
-                    setCalculo({
+                    }else if (num === 'backspace'){
+                    // Exclui o último valor da chave 'primeiroNum' do state calculo
+                    setCalculo({ 
                         'primeiroNum': calculo.primeiroNum.slice(0, -1),
                         'operador': '',
                         'segundoNum': ''
                     })
                     setNumeroDisplay1(numeroDisplay1)   
-                }else{
+                }else{ 
+                    // Adiciona o numero na tela/objeto
                     calculo.primeiroNum += num
                     setNumeroDisplay1(numeroDisplay1 + num)
                     calculo.ultimoNumero = ''
                 }
             }else{
                 if (num === '.'){
+                    // Adicionando ponto para valor Float
                     calculo.segundoNum += num
                     setNumeroDisplay2(numeroDisplay2 + num)
                 }else if(num === 'backspace'){
+                    // Exclui o último valor da chave 'segundoNum' do state calculo e mantem os outros
                     setCalculo({
                         'primeiroNum': calculo.primeiroNum,
                         'operador': calculo.operador,
                         'segundoNum': calculo.segundoNum.slice(0, -1),
                     })
                 }else{
+                    // Adiciona o numero na tela/objeto
                     calculo.segundoNum += num
                     setNumeroDisplay2(numeroDisplay2 + num)
                 }
             }
         }
 
+        // Função para receber o operador clicado
         const recebeOperador = (num) => {
             calculo['operador'] = num
             setOperadorClicado(num)
             setOperador(true)
             setOperador2(false)
-
+    
+            // Verifica se ja houve o primeiro clique no '=', se tiver, ele vai adicionar o ultimo numero e a operação
+            // para fazer outro cálculo.
             if(primeiroClique){
                 setCalculo({
                 'primeiroNum': calculo.ultimoNumero,
@@ -79,7 +94,9 @@ function Calculadora(){
             setPrimeiroClique(true)
         }
 
+        // Função para fazer o calculo com os valores recebidos
         const fazOperacao = (num) => {
+            // Objeto com as funções respectivas de cada tipo de operação
             const operacoes = {
                 '+': (num1, num2) => (parseFloat(num1) + parseFloat(num2)),
                 '-': (num1, num2) => (parseFloat(num1) - parseFloat(num2)),
@@ -88,29 +105,48 @@ function Calculadora(){
                 '*': (num1, num2) => (parseFloat(num1) * parseFloat(num2)),
             }
             
+            // Jogando o resultado da operação na tela
             let result = operacoes[calculo['operador']](calculo.primeiroNum, calculo.segundoNum)
             calculo.ultimoNumero = result
             setResultado(result)
 
+            // Ajustando os states de verificação
             setOperador2(true)
             setOperador(false)
             setPrimeiroCalculo(true)
         }
 
-        const limpaValores = () => {
-            setCalculo({
-                'primeiroNum': '',
-                'operador': calculo.operador,
-                'segundoNum': '',  
-            })
+        // Função para limpar o display e valores do objeto, para a próxima operação
+        const limpaValores = (num, calculaDnv) => {
+            // Esse IF vai verificar se ja tiver um resultado na tela e clicar em um número, ele vai limpar e colocar o numero digitado
+            if(calculaDnv){ 
+                setCalculo({
+                    'primeiroNum': num,
+                    'operador': calculo.operador,
+                    'segundoNum': '',  
+                })
 
-            setPrimeiroClique(false)
-            setResultado('')
-            setNumeroDisplay1('')
-            setNumeroDisplay2('')
-            setOperadorClicado('')
+                setPrimeiroClique(false)
+                setResultado('')
+                setNumeroDisplay1('')
+                setNumeroDisplay2('')
+                setOperadorClicado('')
+            }else{
+                setCalculo({
+                    'primeiroNum': '',
+                    'operador': calculo.operador,
+                    'segundoNum': '',  
+                })
+    
+                setPrimeiroClique(false)
+                setResultado('')
+                setNumeroDisplay1('')
+                setNumeroDisplay2('')
+                setOperadorClicado('')
+            }
         }
         
+        // Função para mostrar mensagem de erro
         const mostraError = () => {
             setResultado('Error')
         }
